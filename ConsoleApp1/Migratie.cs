33ec74cf52727cd4ed2d17ac9b26192e6b82a533
Vlad Pavlyuk <0956458@hr.nl>
2018-03-06 14:08:38 +0100

    made the parsing more generic and made the attributes nullable

--- ConsoleApp1/Migratie.cs
+++ ConsoleApp1/Migratie.cs
@@ -35,76 +35,76 @@
         public string Perioden { get; set; }
 
         [JsonProperty("BeroepsEnNietBeroepsbevolking_1")]
-        public long BeroepsEnNietBeroepsbevolking1 { get; set; }
+        public long? BeroepsEnNietBeroepsbevolking1 { get; set; }
 
         [JsonProperty("Beroepsbevolking_2")]
-        public long Beroepsbevolking2 { get; set; }
+        public long? Beroepsbevolking2 { get; set; }
 
         [JsonProperty("WerkzameBeroepsbevolking_3")]
-        public long WerkzameBeroepsbevolking3 { get; set; }
+        public long? WerkzameBeroepsbevolking3 { get; set; }
 
         [JsonProperty("Werknemer_4")]
-        public long Werknemer4 { get; set; }
+        public long? Werknemer4 { get; set; }
 
         [JsonProperty("WerknemerMetVasteArbeidsrelatie_5")]
-        public long WerknemerMetVasteArbeidsrelatie5 { get; set; }
+        public long? WerknemerMetVasteArbeidsrelatie5 { get; set; }
 
         [JsonProperty("WerknemerMetFlexibeleArbeidsrelatie_6")]
-        public long WerknemerMetFlexibeleArbeidsrelatie6 { get; set; }
+        public long? WerknemerMetFlexibeleArbeidsrelatie6 { get; set; }
 
         [JsonProperty("Zelfstandige_7")]
-        public long Zelfstandige7 { get; set; }
+        public long? Zelfstandige7 { get; set; }
 
         [JsonProperty("ZelfstandigeZonderPersoneelZzp_8")]
-        public long ZelfstandigeZonderPersoneelZzp8 { get; set; }
+        public long? ZelfstandigeZonderPersoneelZzp8 { get; set; }
 
         [JsonProperty("ZelfstandigeMetPersoneel_9")]
-        public long ZelfstandigeMetPersoneel9 { get; set; }
+        public long? ZelfstandigeMetPersoneel9 { get; set; }
 
         [JsonProperty("MeewerkendGezinslid_10")]
-        public long MeewerkendGezinslid10 { get; set; }
+        public long? MeewerkendGezinslid10 { get; set; }
 
         [JsonProperty("PositieInDeWerkkringOnbekend_11")]
-        public long PositieInDeWerkkringOnbekend11 { get; set; }
+        public long? PositieInDeWerkkringOnbekend11 { get; set; }
 
         [JsonProperty("Deeltijd_12")]
-        public long Deeltijd12 { get; set; }
+        public long? Deeltijd12 { get; set; }
 
         [JsonProperty("MinderDan20UurPerWeek_13")]
-        public long MinderDan20UurPerWeek13 { get; set; }
+        public long? MinderDan20UurPerWeek13 { get; set; }
 
         [JsonProperty("MinderDan12UurPerWeek_14")]
-        public long MinderDan12UurPerWeek14 { get; set; }
+        public long? MinderDan12UurPerWeek14 { get; set; }
 
         [JsonProperty("k_12Tot20UurPerWeek_15")]
-        public long K12Tot20UurPerWeek15 { get; set; }
+        public long? K12Tot20UurPerWeek15 { get; set; }
 
         [JsonProperty("k_20Tot35UurPerWeek_16")]
-        public long K20Tot35UurPerWeek16 { get; set; }
+        public long? K20Tot35UurPerWeek16 { get; set; }
 
         [JsonProperty("k_20Tot28UurPerWeek_17")]
-        public long K20Tot28UurPerWeek17 { get; set; }
+        public long? K20Tot28UurPerWeek17 { get; set; }
 
         [JsonProperty("k_28Tot35UurPerWeek_18")]
-        public long K28Tot35UurPerWeek18 { get; set; }
+        public long? K28Tot35UurPerWeek18 { get; set; }
 
         [JsonProperty("Voltijd_19")]
-        public long Voltijd19 { get; set; }
+        public long? Voltijd19 { get; set; }
 
         [JsonProperty("WerklozeBeroepsbevolking_20")]
-        public long WerklozeBeroepsbevolking20 { get; set; }
+        public long? WerklozeBeroepsbevolking20 { get; set; }
 
         [JsonProperty("Werkloosheidspercentage_21")]
-        public double Werkloosheidspercentage21 { get; set; }
+        public double? Werkloosheidspercentage21 { get; set; }
 
         [JsonProperty("NietBeroepsbevolking_22")]
-        public long NietBeroepsbevolking22 { get; set; }
+        public long? NietBeroepsbevolking22 { get; set; }
 
         [JsonProperty("BrutoArbeidsparticipatie_23")]
-        public double BrutoArbeidsparticipatie23 { get; set; }
+        public double? BrutoArbeidsparticipatie23 { get; set; }
 
         [JsonProperty("NettoArbeidsparticipatie_24")]
-        public double NettoArbeidsparticipatie24 { get; set; }
+        public double? NettoArbeidsparticipatie24 { get; set; }
     }
     public partial class Value
     {
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace WebAPIClient
 {
@@ -8,26 +10,19 @@ namespace WebAPIClient
         public static Root GenerateFromJson(string api, string queryoption)
         {
             var url = api + "?" + queryoption;
-            using (var clientjs = new HttpClient())
+            var temp =                           GetRequest(url);
+            var leeftijd =                       GetRequest("https://opendata.cbs.nl/ODataApi/OData/82809NED/Leeftijd");
+            var geslacht =                       GetRequest("https://opendata.cbs.nl/ODataApi/OData/82809NED/Geslacht");
+            var migratieachtergrond =            GetRequest("https://opendata.cbs.nl/ODataApi/OData/82809NED/Migratieachtergrond");
+            foreach (var entry in temp.Value)
             {
-                HttpResponseMessage responseJson = clientjs.GetAsync(url).Result;  
-                responseJson.EnsureSuccessStatusCode();
-                var responseBody = responseJson.Content.ReadAsStringAsync().Result;
-                var temp = Value.FromJson(responseBody);
-                HttpResponseMessage leeftijdJson =
-                    clientjs.GetAsync("https://opendata.cbs.nl/ODataApi/OData/82809NED/Leeftijd").Result;
-                leeftijdJson.EnsureSuccessStatusCode();
-                var leeftijdBody = leeftijdJson.Content.ReadAsStringAsync().Result;
-                var tmpleeftijd = Value.FromJson(leeftijdBody);
-                foreach (var entry in temp.Value)
-                {
-                    entry.Leeftijd = ConvertToString(entry.Leeftijd.ToString(), tmpleeftijd);
-                    entry.Geslacht = ConvertValue(entry.Geslacht);
-                    entry.Migratieachtergrond = ConvertValue(entry.Migratieachtergrond);
-                }
-
-                return temp;
+                entry.Leeftijd =                 ConvertToString(leeftijd,entry.Leeftijd);
+                entry.Geslacht =                 ConvertToString(geslacht,entry.Geslacht);
+                entry.Migratieachtergrond =      ConvertToString(migratieachtergrond,entry.Migratieachtergrond);
+                //entry.Units =                toDictionary(entry,dataproperties,entry.Beroepsbevolking2);
+                
             }
+            return temp;
         }
         static void Main(string[] args)
         {
@@ -40,31 +35,23 @@ namespace WebAPIClient
                 Console.WriteLine(entry.Leeftijd);
                 Console.WriteLine("\n");
             }
-
         }
-
-        private static string ConvertToString(string value, Root leeftijd)
+        private static string ConvertToString(Root root, string target)
         {
-            foreach (var entry in leeftijd.Value)
+            foreach (var entry in root.Value)
             {
-                if (value == entry.Key)
+                if (target == entry.Key)
                     return entry.Title;
             }
             return null;
         }
-        private static string ConvertValue(string gender)
+        private static Root GetRequest(string url)
         {
-            switch (gender)
-            {
-                case "T001038": return "Mannen en Vrouwen";
-                case "3000   ": return "Mannen";
-                case "4000   ": return "Vrouwen";
-                case "T001040": return "Totaal ";
-                case "2012605": return "Met migratieachtergrond";
-                case "2012655": return "Westerse migratieachtergrond";
-                case "2012657": return "Niet-westerse migratieachtergrond";
-                default: return null;
-            }
+            var client = new HttpClient();
+            HttpResponseMessage responseJson = client.GetAsync(url).Result;  
+            responseJson.EnsureSuccessStatusCode();
+            var responseBody = responseJson.Content.ReadAsStringAsync().Result;
+            return Value.FromJson(responseBody);
         }
     }
 }
