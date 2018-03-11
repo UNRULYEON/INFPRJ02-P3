bf5da61e6704a9117bb4458fafcd81d926082bd7
Amar Kisoensingh <unrulyeon314@gmail.com>
2018-03-11 13:54:20 +0100

    Reinitialized dotnet project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace INFPRJ02_P3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
