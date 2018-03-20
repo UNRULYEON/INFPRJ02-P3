07b7c28d68e8769da1ef4b79f321ef3a7da6854f
Vlad Pavlyuk <0956458@hr.nl>
2018-03-20 13:48:59 +0100

    Created deelvraag 1 graph

---
+++ MvcMovie/Controllers/deelvraag1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace MvcMovie
+{
+    public class deelvraag1 : Controller
+    {
+        public IActionResult Index(Deelvraag1FilterOptions filter)
+        {
+            if (filter != null)
+            {
+                //var migratie = "https://opendata.cbs.nl/ODataApi/OData/82809NED/TypedDataSet?$top=1000&" + age + " and " + gender + " and " + background + " and " + period;
+                ViewData["root"] = "https://opendata.cbs.nl/ODataApi/OData/83599NED/TypedDataSet?";
+                ViewData["bedrijfstak"] = filter.Bedrijfstak;
+                ViewData["regio"] = filter.Regio;
+                ViewData["period"] = filter.Period;
+               /* var migratie2 = HttpUtility.HtmlEncode("https://opendata.cbs.nl/ODataApi/OData/82809NED/TypedDataSet?$top=1000&" + age + " and " + gender + " and " + background2 + " and " + period);
+                ViewData["migratie"] = migratie;
+                ViewData["migratie2"] = migratie2;
+                ViewBag.Mig = migratie;*/
+            }
+            var filters = new Deelvraag1FilterOptions()
+            {
+                SelectedBedrijfstak = MvcMovie.Request.CreateList("83599NED/BedrijfstakkenBranchesSBI2008"),
+                SelectedRegio = MvcMovie.Request.CreateList("83599NED/RegioS"),
+                SelectedPeriod = MvcMovie.Request.CreateList("83599NED/Perioden"),
+            };
+            ViewData["Filters"] = filters;
+            return View();
+        }
+    }
+}
