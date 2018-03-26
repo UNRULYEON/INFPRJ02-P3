0290b0ab25847f0692e13b2afce25e93ebc30c14
Doritosxl <doritosvslays@gmail.com>
2018-03-26 12:38:37 +0200

    controller index en filteroptions aangepast voor deelvraag 2

--- MvcMovie/Controllers/deelvraag2.cs
+++ MvcMovie/Controllers/deelvraag2.cs
@@ -13,16 +13,19 @@ namespace MvcMovie
         {
             if (filter != null)
             {
-                ViewData["root"] = "https://opendata.cbs.nl/ODataApi/odata/81953NED/TypedDataSet?";
-                ViewData["waardeklas"] = filter.WOZWaardeklassen;
-                ViewData["bruto"] = filter.BrutoInkomen;
-                ViewData["Huishouden"] = filter.Huishoudens;
+                //string KenmerkenBaanBedrijfsgrootte = "$filter=KenmerkenBaanBedrijfsgrootte eq " + "'" + filter.KenmerkenBaanBedrijfsgrootte +  "'";
+                string Perioden = "Perioden eq " + "'" + filter.Perioden + "'";
+
+                ViewData["root"] = "https://opendata.cbs.nl/ODataApi/odata/81405ned/TypedDataSet?";
+                ViewData["KenmerkenBaanBedrijfsgrootte"] = filter.KenmerkenBaanBedrijfsgrootte;
+                ViewData["Perioden"] = filter.Perioden;
             }
             var filters = new Deelvraag2FilterOptions()
             {
-                selectedBrutoInkomen = MvcMovie.Request.CreateList("81953NED/BrutoInkomen"),
-                selectedPerioden = MvcMovie.Request.CreateList("81953NED/Perioden"),
-                selectedRegioS = MvcMovie.Request.CreateList("81953NED/RegioS?$filter=endswith(Title,'PV)') eq true"),
+                selectedBedrijfstakkenBranchesSBI2008 = MvcMovie.Request.CreateList("81405ned/BedrijfstakkenBranchesSBI2008?$filter=Key eq '300035' or Key eq '391600' or Key eq '300010' or Key eq '419000' or Key eq '422400'"),
+                selectedKenmerkenBaanBedrijfsgrootte = MvcMovie.Request.CreateList("81405ned/KenmerkenBaanBedrijfsgrootte?$filter=Key%20eq%20%2710000%27%20or%20Key%20eq%20%2721260%27%20or%20Key%20eq%20%2721250%27%20or%20Key%20eq%20%2721210%27 or Key eq '21260' or Key eq '21250' or Key eq '601  ' or Key eq '602  ' or Key eq '604  ' or Key eq '20360' or Key eq '20240'"),
+                selectedPerioden = MvcMovie.Request.CreateList("81405ned/Perioden"),
+                selectedCritters = MvcMovie.Request.CreateList("81405ned/DataProperties?select=Key,Title,Description&$filter=Key eq 'Minimumloon_2' or Key eq 'TotaalArbeidsvolume_8' or Key eq 'TotaalBanen_1' or Key eq 'Minimumloon_9'")
             };
             ViewData["Filters"] = filters;
             return View();
--- MvcMovie/Models/FilterOptions.cs
+++ MvcMovie/Models/FilterOptions.cs
@@ -30,17 +30,28 @@ namespace Models
 
     public class Deelvraag2FilterOptions
     {
-        public string WOZWaardeklassen { get; set; }
-        public List<SelectListItem> selectedWOZWaardeklassen { get; set; }
-        public string BrutoInkomen { get; set; }
-        public List<SelectListItem> selectedBrutoInkomen { get; set; }
-        public string Huishoudens { get; set; }
-        public List<SelectListItem> selectedHuishoudens { get; set; }
-        public string Herkomst { get; set; }
-        public List<SelectListItem> selectedHerkomst { get; set; }
-        public string RegioS { get; set; }
-        public List<SelectListItem> selectedRegioS { get; set; }
+        //public string WOZWaardeklassen { get; set; }
+        //public List<SelectListItem> selectedWOZWaardeklassen { get; set; }
+        //public string BrutoInkomen { get; set; }
+        //public List<SelectListItem> selectedBrutoInkomen { get; set; }
+        //public string Huishoudens { get; set; }
+        //public List<SelectListItem> selectedHuishoudens { get; set; }
+        //public string Herkomst { get; set; }
+        //public List<SelectListItem> selectedHerkomst { get; set; }
+        //public string RegioS { get; set; }
+        //public List<SelectListItem> selectedRegioS { get; set; }
+        //public string Perioden { get; set; }
+        //public List<SelectListItem> selectedPerioden { get; set; }
+
         public string Perioden { get; set; }
         public List<SelectListItem> selectedPerioden { get; set; }
+        public string KenmerkenBaanBedrijfsgrootte { get; set; }
+        public List<SelectListItem> selectedKenmerkenBaanBedrijfsgrootte { get; set; }
+        public string BedrijfstakkenBranchesSBI2008 { get; set; }
+        public List<SelectListItem> selectedBedrijfstakkenBranchesSBI2008 { get; set; }
+        public string BedrijfstakkenBranchesSBI20082 { get; set; }
+        public List<SelectListItem> selectedBedrijfstakkenBranchesSBI20082 { get; set; }
+        public string critters { get; set; }
+        public List<SelectListItem> selectedCritters { get; set; }
     }
 }
